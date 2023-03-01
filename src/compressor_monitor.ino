ec9c297d5db0de075c82c9b8c2e74c948965646e
Michael Keller <github@ike.ch>
2023-03-01 18:56:20 +1300

    Add remaining time until full estimation.
    Based on past pressure increase - needs testing for accuracy.
    Also extended the JSON returned on the web API to include the estimate
    and an array of warnings.

--- src/compressor_monitor.ino
+++ src/compressor_monitor.ino
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <assert.h>
 
 #include <TFT_eSPI.h>
 
@@ -43,17 +44,19 @@ WebServer webServer(80);
 
 #define DATA_UPDATE_FREQUENCY_HZ 5
 
-#define DATA_BUFFER_SIZE 512
+#define DATA_BUFFER_SIZE 1024
 DynamicJsonDocument dataJson(DATA_BUFFER_SIZE);
 char getDataResponse[DATA_BUFFER_SIZE];
 
 #endif // WIFI_x_SSID
 
-#if !defined(SHOW_IGNITION)
-#undef HAS_SWITCH
+#if !defined(USE_IGNITION)
+#undef USE_RELAIS
 #endif
 
 #define FREQUENCY_TO_MS(frequencyHz) (1000 / (frequencyHz))
+#define MS_PER_S 1000
+#define MS_PER_MINUTE (60 * MS_PER_S)
 
 #define LOOP_FREQUENCY_HZ 200
 #define DISPLAY_UPDATE_FREQUENCY_HZ 5
@@ -61,12 +64,20 @@ char getDataResponse[DATA_BUFFER_SIZE];
 
 #define STARTUP_DELAY_MS 5000
 
+#define FILL_RATE_SAMPLING_TIME_S 60
+static_assert(FILL_RATE_SAMPLING_TIME_S % 2 == 0, "Fill rate sampling time must be an even number.");
+#define FILL_RATE_SAMPLE_COUNT (FILL_RATE_SAMPLING_TIME_S + 1)
+#define FILL_RATE_MAXIMUM_VARIANCE 0.3f
+
 #define BUTTON_REPEAT_DELAY_MS 500
 #define BUTTON_REPEAT_INTERVAL_MS 100
 
 #define BEEPER_SEQUENCE_LENGTH 20
 
-#define ORANGE 0xFBE0
+#undef TFT_ORANGE
+#define TFT_ORANGE 0xFBE0
+#define TFT_GREY 0x5AEB
+
 
 typedef struct beeperSequence_s {
     uint8_t period;
@@ -74,7 +85,7 @@ typedef struct beeperSequence_s {
     bool sequence[BEEPER_SEQUENCE_LENGTH];
 } beeperSequence_t;
 
-#if defined(SHOW_IGNITION)
+#if defined(USE_IGNITION)
 typedef enum {
     IGNITION_STATE_OFF = 0,
     IGNITION_STATE_ON,
@@ -97,7 +108,7 @@ typedef enum {
     PRESSURE_STATE_FILLING = 0,
     PRESSURE_STATE_APPROACHING,
     PRESSURE_STATE_OVER,
-#if defined(HAS_SWITCH)
+#if defined(USE_RELAIS)
     PRESSURE_STATE_SAFETY_STOPPED,
 #endif
     PRESSURE_STATE_COUNT
@@ -107,7 +118,7 @@ const char *pressureStateNames[] = {
     "FILL",
     "CLOSE",
     "OVER",
-#if defined(HAS_SWITCH)
+#if defined(USE_RELAIS)
     "STOP",
 #endif
 };
@@ -115,8 +126,8 @@ const char *pressureStateNames[] = {
 const uint16_t pressureStateColours[] = {
     TFT_GREEN,
     TFT_YELLOW,
-    ORANGE,
-#if defined(HAS_SWITCH)
+    TFT_ORANGE,
+#if defined(USE_RELAIS)
     TFT_RED,
 #endif
 };
@@ -134,7 +145,7 @@ const beeperSequence_t pressureStateBeeperSequences[PRESSURE_STATE_COUNT] = {
         .period = 1,
         .offset = 0,
         .sequence = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
-#if defined(HAS_SWITCH)
+#if defined(USE_RELAIS)
     }, {
         .period = 3,
         .offset = 0,
@@ -159,9 +170,9 @@ const beeperSequence_t beeperSequenceBatteryLow = {
 
 typedef enum {
     INPUT_STATE_PRESSURE_LIMIT = 0,
-#if defined(SHOW_IGNITION)
+#if defined(USE_IGNITION)
     INPUT_STATE_IGNITION,
-#if defined(HAS_SWITCH)
+#if defined(USE_RELAIS)
     INPUT_STATE_OVERRIDE,
 #endif
 #endif
@@ -171,16 +182,16 @@ typedef enum {
 
 const char *inputStateNames[] = {
     "LIMIT",
-#if defined(SHOW_IGNITION)
+#if defined(USE_IGNITION)
     "IGNITION",
-#if defined(HAS_SWITCH)
+#if defined(USE_RELAIS)
     "OVERR",
 #endif
 #endif
     "PURGE",
 };
 
-#if defined(HAS_SWITCH)
+#if defined(USE_RELAIS)
 const beeperSequence_t beeperSequenceOverrideActive = {
     .period = 1,
     .offset = 0,
@@ -224,11 +235,11 @@ const char *serverTypeNames[] = { "?", "C", "A" };
 typedef struct globalState_s {
     float pressureBar;
     float batteryV;
-#if defined(HAS_SWITCH)
+    uint8_t pressureLimitBar;
+#if defined(USE_IGNITION)
+#if defined(USE_RELAIS)
     uint64_t overrideCountdownStartedMs;
 #endif
-    uint8_t pressureLimitBar;
-#if defined(SHOW_IGNITION)
     ignitionState_t ignitionState;
 #endif
     pressureState_t pressureState;
@@ -240,6 +251,7 @@ typedef struct globalState_s {
     Button2 *repeatEventButton;
     serverState_t serverState;
     serverType_t serverType;
+    int32_t timeUntilFullEstimateMs;
 } globalState_t;
 
 globalState_t state;
@@ -294,7 +306,7 @@ void handleUpDownButtonPressed(Button2 &button)
         handlePressureLimitChange(button);
 
         break;
-#if defined(SHOW_IGNITION)
+#if defined(USE_IGNITION)
     case INPUT_STATE_IGNITION:
         if (button == buttonUp) {
             state.ignitionState = IGNITION_STATE_ON;
@@ -307,7 +319,7 @@ void handleUpDownButtonPressed(Button2 &button)
         }
 
         break;
-#if defined(HAS_SWITCH)
+#if defined(USE_RELAIS)
     case INPUT_STATE_OVERRIDE:
         if (button == buttonUp) {
             state.overrideCountdownStartedMs = millis();
@@ -438,6 +450,7 @@ void setup(void)
     setupAdc();
 
     state.pressureLimitBar = DEFAULT_PRESSURE_LIMIT_BAR;
+    state.timeUntilFullEstimateMs = -1;
 
     state.serverState = SERVER_STATE_WIFI_DISCONNECTED;
     state.serverType = SERVER_TYPE_NONE;
@@ -449,7 +462,7 @@ void setup(void)
     pinMode(LED_1_PIN, OUTPUT);
     digitalWrite(LED_1_PIN, 0);
 
-#if defined(HAS_SWITCH)
+#if defined(USE_RELAIS)
     pinMode(RELAIS_1_PIN, OUTPUT);
     pinMode(RELAIS_2_PIN, OUTPUT);
 
@@ -462,7 +475,7 @@ void setup(void)
 
 int64_t getTimeUntilPurgeMs(void)
 {
-    return state.lastPurgeRunTimeMs + 1000 * PURGE_INTERVAL_S - state.runTimeMs;
+    return state.lastPurgeRunTimeMs + PURGE_INTERVAL_S * MS_PER_S - state.runTimeMs;
 }
 
 void readSensors(const uint64_t currentTimeMs)
@@ -494,12 +507,50 @@ void readSensors(const uint64_t currentTimeMs)
     }
 }
 
+void updateTimeUntilFullEstimate(const uint64_t currentTimeMs)
+{
+    static float pressureSamplesBar[FILL_RATE_SAMPLE_COUNT];
+    static uint64_t nextSampleTimeMs = 0;
+    static unsigned lastEnteredSample = 0;
+    static uint64_t lastEstimateMs = 0;
+
+    float lastPressureBar = pressureSamplesBar[lastEnteredSample];
+    while (nextSampleTimeMs <= currentTimeMs) {
+        lastEnteredSample = (lastEnteredSample + 1) % FILL_RATE_SAMPLE_COUNT;
+        if (nextSampleTimeMs / MS_PER_S < currentTimeMs / MS_PER_S) {
+            // Crude interpolation
+            pressureSamplesBar[lastEnteredSample] = (lastPressureBar + state.pressureBar) / 2;
+        } else {
+            pressureSamplesBar[lastEnteredSample] = state.pressureBar;
+        }
+
+        nextSampleTimeMs = nextSampleTimeMs + MS_PER_S;
+    }
+
+    float pressureUntilFullBar = state.pressureLimitBar - state.pressureBar;
+    if (pressureUntilFullBar <= 0) {
+        state.timeUntilFullEstimateMs = 0;
+        lastEstimateMs = 0;
+    } else {
+        float firstHalfFillVolumeBar = pressureSamplesBar[(lastEnteredSample - (FILL_RATE_SAMPLING_TIME_S / 2) + FILL_RATE_SAMPLE_COUNT) % FILL_RATE_SAMPLE_COUNT] - pressureSamplesBar[(lastEnteredSample - FILL_RATE_SAMPLING_TIME_S + FILL_RATE_SAMPLE_COUNT) % FILL_RATE_SAMPLE_COUNT];
+        float secondHalfFillVolumeBar = pressureSamplesBar[lastEnteredSample] - pressureSamplesBar[(lastEnteredSample - (FILL_RATE_SAMPLING_TIME_S / 2) + FILL_RATE_SAMPLE_COUNT) % FILL_RATE_SAMPLE_COUNT];
+
+        if (firstHalfFillVolumeBar > 0 && secondHalfFillVolumeBar >= (1 - FILL_RATE_MAXIMUM_VARIANCE) * firstHalfFillVolumeBar && secondHalfFillVolumeBar <= (1 + FILL_RATE_MAXIMUM_VARIANCE) * firstHalfFillVolumeBar) {
+            float fillRateBarPerMin = (firstHalfFillVolumeBar + secondHalfFillVolumeBar) * (60 / FILL_RATE_SAMPLING_TIME_S);
+            state.timeUntilFullEstimateMs = pressureUntilFullBar / (fillRateBarPerMin / MS_PER_MINUTE);
+            lastEstimateMs = currentTimeMs;
+        } else if (currentTimeMs - lastEstimateMs > (FILL_RATE_SAMPLING_TIME_S / 2) * MS_PER_S) {
+            state.timeUntilFullEstimateMs = -1;
+        }
+    }
+}
+
 void updateState(const uint64_t currentTimeMs)
 {
     static bool lastPurgeNeededState = false;
-#if defined(SHOW_IGNITION)
+#if defined(USE_IGNITION)
     static uint64_t lastRunTimeUpdateMs = 0;
-#if defined(HAS_SWITCH)
+#if defined(USE_RELAIS)
     static pressureState_t lastPressureState = PRESSURE_STATE_FILLING;
 #endif
 
@@ -527,7 +578,7 @@ void updateState(const uint64_t currentTimeMs)
         lastPurgeNeededState = false;
     }
 
-#if defined(HAS_SWITCH)
+#if defined(USE_RELAIS)
     if (state.pressureBar >= state.pressureLimitBar + PRESSURE_STOP_THRESHOLD_BAR && !state.overrideCountdownStartedMs) {
         state.pressureState = PRESSURE_STATE_SAFETY_STOPPED;
         state.ignitionState = IGNITION_STATE_OFF;
@@ -538,14 +589,14 @@ void updateState(const uint64_t currentTimeMs)
 #endif
     if (state.pressureBar >= state.pressureLimitBar) {
         state.pressureState = PRESSURE_STATE_OVER;
-#if defined(HAS_SWITCH)
+#if defined(USE_RELAIS)
         if (lastPressureState != state.pressureState) {
             state.inputState = INPUT_STATE_OVERRIDE;
         }
 #endif
-    } else if (state.pressureBar >= state.pressureLimitBar - PRESSURE_APPROACHING_THRESHOLD_BAR) {
+    } else if ((state.timeUntilFullEstimateMs != -1 && state.timeUntilFullEstimateMs < WARN_TIME_S * MS_PER_S) || state.pressureBar >= state.pressureLimitBar - PRESSURE_APPROACHING_THRESHOLD_BAR) {
         state.pressureState = PRESSURE_STATE_APPROACHING;
-#if defined(HAS_SWITCH)
+#if defined(USE_RELAIS)
         if (lastPressureState != state.pressureState) {
             state.inputState = INPUT_STATE_OVERRIDE;
         }
@@ -554,12 +605,12 @@ void updateState(const uint64_t currentTimeMs)
         state.pressureState = PRESSURE_STATE_FILLING;
     }
 
-#if defined(HAS_SWITCH)
+#if defined(USE_RELAIS)
     if (state.overrideCountdownStartedMs) {
         if (state.ignitionState == IGNITION_STATE_OFF) {
             state.overrideCountdownStartedMs = 0;
         } else {
-            if (currentTimeMs - state.overrideCountdownStartedMs >= OVERRIDE_DURATION_S * 1000) {
+            if (currentTimeMs - state.overrideCountdownStartedMs >= OVERRIDE_DURATION_S * MS_PER_S) {
                 state.overrideCountdownStartedMs = 0;
             }
         }
@@ -572,10 +623,10 @@ void updateState(const uint64_t currentTimeMs)
         state.batteryState = BATTERY_STATE_OK;
     }
 
-#if defined(HAS_SWITCH)
+#if defined(USE_RELAIS)
     lastPressureState = state.pressureState;
 
-    if (timeUntilPurgeMs <= -1000 * PURGE_GRACE_TIME_S) {
+    if (timeUntilPurgeMs <= -PURGE_GRACE_TIME_S * MS_PER_S) {
         state.ignitionState = IGNITION_STATE_OFF;
     }
 #endif
@@ -589,7 +640,7 @@ void updateState(const uint64_t currentTimeMs)
     }
 }
 
-#if defined(HAS_SWITCH)
+#if defined(USE_RELAIS)
 void updateOutput(void)
 {
     digitalWrite(RELAIS_1_PIN, !state.ignitionState);
@@ -619,9 +670,9 @@ void updateBeeper(const uint64_t currentTimeMs)
 
         bool beeperOn = false;
 
-#if defined(HAS_SWITCH)
+#if defined(USE_RELAIS)
         if (state.overrideCountdownStartedMs) {
-            if (currentTimeMs - state.overrideCountdownStartedMs >= (OVERRIDE_DURATION_S - 10) * 1000) {
+            if (currentTimeMs - state.overrideCountdownStartedMs >= (OVERRIDE_DURATION_S - 10) * MS_PER_S) {
                 beeperOn = beeperOn || needsBeeperOn(beeperSequenceOverrideEnding, period, position);
             } else {
                 beeperOn = beeperOn || needsBeeperOn(beeperSequenceOverrideActive, period, position);
@@ -629,7 +680,7 @@ void updateBeeper(const uint64_t currentTimeMs)
         } else
 #endif
         {
-#if defined(SHOW_IGNITION)
+#if defined(USE_IGNITION)
             if (state.ignitionState == IGNITION_STATE_OFF) {
                 beeperOn = beeperOn || needsBeeperOn(beeperSequenceIgnitionOff, period, position);
             }
@@ -658,7 +709,7 @@ void updateDisplay(const uint64_t currentTimeMs)
 Geometry for 320 x 240 display:
 
 Top row,
-0, 0, 319, 239
+0, 0, 319, 69
 
 Left column:
 0, 70, 159, 239, in rows of 27 height
@@ -679,7 +730,7 @@ Right column:
 #define ROW_6 205
 
 #define COL_HEADING_1 10
-#define COL_1 38
+#define COL_1 42
 #define COL_HEADING_2 170
 #define COL_2 198
 
@@ -696,10 +747,13 @@ Right column:
         tft.fillRect(COL_HEADING_2, ROW_5, 28, 27, TFT_BLACK);
 
         tft.setTextSize(3);
-
         tft.setTextColor(pressureStateColours[state.pressureState]);
         tft.setCursor(HEADER_X, HEADER_Y);
         tft.printf("%.1f", state.pressureBar);
+
+        tft.setTextSize(2);
+        tft.setCursor(235, 20);
+        tft.print("bar");
 
         tft.setTextSize(1);
 
@@ -714,10 +768,10 @@ Right column:
         tft.setCursor(COL_1, ROW_2);
         tft.printf("%d bar", state.pressureLimitBar);
 
-#if defined(SHOW_IGNITION)
-#if defined(HAS_SWITCH)
+#if defined(USE_IGNITION)
+#if defined(USE_RELAIS)
         if (state.overrideCountdownStartedMs) {
-            if (currentTimeMs - state.overrideCountdownStartedMs >= (OVERRIDE_DURATION_S - 10) * 1000) {
+            if (currentTimeMs - state.overrideCountdownStartedMs >= (OVERRIDE_DURATION_S - 10) * MS_PER_S) {
                 tft.setTextColor(TFT_RED);
             } else {
                 tft.setTextColor(TFT_YELLOW);
@@ -730,9 +784,9 @@ Right column:
         tft.setCursor(COL_HEADING_1, ROW_3);
         tft.print("I:");
         tft.setCursor(COL_1, ROW_3);
-#if defined(HAS_SWITCH)
+#if defined(USE_RELAIS)
         if (state.overrideCountdownStartedMs) {
-            tft.printf("%d s", (int)((state.overrideCountdownStartedMs + 1000 * OVERRIDE_DURATION_S - currentTimeMs) / 1000));
+            tft.printf("%d s", (int)((state.overrideCountdownStartedMs + OVERRIDE_DURATION_S * MS_PER_S - currentTimeMs) / MS_PER_S));
         } else
 #endif
         {
@@ -780,12 +834,12 @@ Right column:
         tft.setCursor(COL_HEADING_2, ROW_1);
         tft.print("T:");
         tft.setCursor(COL_2, ROW_1);
-        tft.printf("%.2f h", state.runTimeMs / 1000.0 / 3600);
+        tft.printf("%.2f h", state.runTimeMs / (60.0 * MS_PER_MINUTE));
 
         int64_t timeUntilPurgeMs = getTimeUntilPurgeMs();
         if (timeUntilPurgeMs <= 0) {
             tft.setTextColor(TFT_RED);
-        } else if (timeUntilPurgeMs <= 60000) {
+        } else if (timeUntilPurgeMs <= WARN_TIME_S * MS_PER_S) {
             tft.setTextColor(TFT_YELLOW);
         } else {
             tft.setTextColor(TFT_GREEN);
@@ -793,7 +847,28 @@ Right column:
         tft.setCursor(COL_HEADING_2, ROW_2);
         tft.print("P:");
         tft.setCursor(COL_2, ROW_2);
-        tft.printf("%s%d:%02d min", (timeUntilPurgeMs <= 0) ? "-" : "", (int)abs(timeUntilPurgeMs / 60000), (int)abs((timeUntilPurgeMs / 1000) % 60));
+        // Need to print a '-' for negative values even if the minutes are 0, so this looks convoluted
+        tft.printf("%s%d:%02d min", (timeUntilPurgeMs <= 0) ? "-" : "", (int)abs(timeUntilPurgeMs / MS_PER_MINUTE), (int)abs((timeUntilPurgeMs / MS_PER_S) % 60));
+
+        if (state.timeUntilFullEstimateMs == -1) {
+            tft.setTextColor(TFT_GREY);
+        } else if (state.timeUntilFullEstimateMs == 0) {
+            tft.setTextColor(TFT_RED);
+        } else if (state.timeUntilFullEstimateMs <= WARN_TIME_S * MS_PER_S) {
+            tft.setTextColor(TFT_YELLOW);
+        } else {
+            tft.setTextColor(TFT_GREEN);
+        }
+        tft.setCursor(COL_HEADING_2, ROW_3);
+        tft.print("E:");
+        tft.setCursor(COL_2, ROW_3);
+        if (state.timeUntilFullEstimateMs == -1) {
+            tft.print("??");
+        } else if (state.timeUntilFullEstimateMs < 100 * MS_PER_MINUTE) {
+            tft.printf("%d:%02d min", state.timeUntilFullEstimateMs / MS_PER_MINUTE, (state.timeUntilFullEstimateMs / MS_PER_S) % 60);
+        } else {
+            tft.print(">99 min");
+        }
 
         tft.setTextColor(TFT_GREEN);
         tft.setCursor(COL_HEADING_2, ROW_6);
@@ -814,15 +889,17 @@ void updateButtons(void)
 
 void updateDataJson(void)
 {
+    dataJson.clear();
+
     dataJson["pressureBar"] = state.pressureBar;
     dataJson["pressureLimitBar"] = state.pressureLimitBar;
     dataJson["pressureState"] = pressureStateNames[state.pressureState];
-#if defined(SHOW_IGNITION)
+#if defined(USE_IGNITION)
     dataJson["ignitionState"] = ignitionStateNames[state.ignitionState];
 #else
     dataJson["ignitionState"] = "ON";
 #endif
-#if defined(HAS_SWITCH)
+#if defined(USE_RELAIS)
     dataJson["overrideCountdownDurationMs"] = state.overrideCountdownStartedMs ? millis() - state.overrideCountdownStartedMs : 0;
 #else
     dataJson["overrideCountdownDurationMs"] = 0;
@@ -830,6 +907,29 @@ void updateDataJson(void)
     dataJson["runTimeMs"] = state.runTimeMs;
     dataJson["timeUntilPurgeMs"] = getTimeUntilPurgeMs();
     dataJson["batteryV"] = state.batteryV;
+    dataJson["timeUntilFullEstimateMs"] = state.timeUntilFullEstimateMs;
+    dataJson["warnTimeMs"] = WARN_TIME_S * MS_PER_S;
+
+    JsonArray alerts = dataJson["alerts"].to<JsonArray>();
+    if (state.pressureState != PRESSURE_STATE_FILLING) {
+        alerts.add("PRESSURE");
+    }
+#if defined(USE_IGNITION)
+#if defined(USE_RELAIS)
+    if (state.overrideCountdownStartedMs) {
+        alerts.add("COUNTDOWN");
+    }
+#endif
+    if (state.ignitionState == IGNITION_STATE_OFF) {
+        alerts.add("IGNITION_OFF");
+    }
+#endif
+    if (state.batteryState != BATTERY_STATE_OK) {
+        alerts.add("BATTERY");
+    }
+    if (getTimeUntilPurgeMs() <= WARN_TIME_S * MS_PER_S) {
+        alerts.add("PURGE");
+    }
 }
 
 void updateDataResponse(void)
@@ -901,7 +1001,7 @@ void updateWebServer(const uint64_t currentTimeMs)
     case SERVER_STATE_WIFI_DISCONNECTED:
 #if defined(WIFI_CLIENT_SSID)
         WiFi.begin((char *)WIFI_CLIENT_SSID, (char *)WIFI_CLIENT_PASSWORD);
-        delayUntilMs = currentTimeMs + 60000; // wait for WiFi to connect
+        delayUntilMs = currentTimeMs + MS_PER_MINUTE; // wait for WiFi to connect
         state.serverState = SERVER_STATE_WIFI_CONNECTING;
 
         break;
@@ -914,7 +1014,7 @@ void updateWebServer(const uint64_t currentTimeMs)
             WiFi.softAP(WIFI_AP_SSID, WIFI_AP_PASSWORD);
             state.serverType = SERVER_TYPE_AP;
 
-            delayUntilMs = currentTimeMs + 1000; // wait for WiFi to start
+            delayUntilMs = currentTimeMs + MS_PER_S; // wait for WiFi to start
             state.serverState = SERVER_STATE_WIFI_AP_SET_IP;
         }
 
@@ -924,7 +1024,7 @@ void updateWebServer(const uint64_t currentTimeMs)
             IPAddress netmask(WIFI_AP_NETMASK);
             WiFi.softAPConfig(apIp, apIp, netmask);
 
-            delayUntilMs = currentTimeMs + 1000; // wait for WiFi to start
+            delayUntilMs = currentTimeMs + MS_PER_S; // wait for WiFi to start
             state.serverState = SERVER_STATE_WIFI_CONNECTED;
         }
 #endif
@@ -965,9 +1065,11 @@ void loop(void)
 
     readSensors(loopStartMs);
 
+    updateTimeUntilFullEstimate(loopStartMs);
+
     updateState(loopStartMs);
 
-#if defined(HAS_SWITCH)
+#if defined(USE_RELAIS)
     updateOutput();
 #endif
 
